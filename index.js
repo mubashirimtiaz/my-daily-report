8ddd4fe87cefb46738394b1713a79190f771fc8f
mubashirimtiaz <mubashir.imtiaz123@gmail.com>
2021-11-01 18:40:18 +0500

    implemented accuweather api

--- index.js
+++ index.js
@@ -4,7 +4,17 @@ const nodemailer = require('nodemailer');
 
 
 (async () => {
-  let transporter = nodemailer.createTransport({
+
+  const locationRequest = await fetch(`http://dataservice.accuweather.com/locations/v1/cities/PK/search?q=${decodeURIComponent('Karachi')}&apikey=${process.env.ACCUWEATHER_API_KEY}`);  
+  const [locationData] = await locationRequest.json();
+  const locationKey = locationData.Key
+
+  const forecastRequest = await fetch(`http://dataservice.accuweather.com/forecasts/v1/daily/1day/${locationKey}?apikey=${process.env.ACCUWEATHER_API_KEY}`);  
+  const {Headline:{Text:forecastText},DailyForecasts:dailyForecasts} = await forecastRequest.json();
+
+  const temperature = dailyForecasts[0]?.Temperature;
+
+  const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
     port: 465,
     secure: true,
@@ -21,7 +31,13 @@ const nodemailer = require('nodemailer');
     text: `
       Daily Report
     `, // plain text body
-    html: "<b>Daily Report</b>", // html body
+    html: `
+      <h1>Daily Report</h1>
+      <h2>Weather</h2>
+      <p>Forecast: <em>${forecastText}</em></p>
+      <p>Min: <em>${temperature.Mimimum.Value}°${temperature.Mimimum.Unit}</em></p>
+      <p>Max: <em>${temperature.Maximum.Value}°${temperature.Maximum.Unit}</em></p>
+    `, // html body
   });
 
 })();
